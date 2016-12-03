b50503cd927aa3e1b9458d86dcb0166555857b4c
Miguel Pardo <migsub77@gmail.com>
2016-12-02 21:06:26 -0800

    email again

--- contact.php
+++ contact.php
@@ -1,17 +1,54 @@
 <?php
 
-if($_POST["submit"]) {
-    $recipient="migsub77@gmail.com";
-    $subject="Form to email message";
-    $sender=$_POST["sender"];
-    $senderEmail=$_POST["senderEmail"];
-    $message=$_POST["message"];
+	// site owner
+	$site_name = 'migpardo.com';
+	$sender_domain = 'migsub77@gmail.com';
+	$to = 'migsub77@gmail.com';
 
-    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+	// contact form fields
+	$name = trim( $_POST['name'] );
+	$email = trim( $_POST['email'] );
+	$subject = trim( $_POST['subject'] );
+	$message = trim( $_POST['message'] );
 
-    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+	// check for error
+	$error = false;
+	if ( $name === "" ) { $error = true; }
+	if ( $email === "" ) { $error = true; }
+	if ( $subject === "" ) { $error = true; }
+	if ( $message === "" ) { $error = true; }
 
-    $thankYou="<p>Thank you! Your message has been sent.</p>";
-}
+	// anti-spam check
+	// http://nfriedly.com/techblog/2009/11/how-to-build-a-spam-free-contact-forms-without-captchas/
+	// if the url field is empty
+	if(isset($_POST['url']) && $_POST['url'] == ''){
+
+		 // if no error, then send mail
+		if ( $error == false )
+		{
+			$body = "Name: $name \n\nEmail: $email \n\nMessage: $message";
+
+			$headers = "From: " . $site_name . ' <' . $sender_domain . '> ' . "\r\n";
+			$headers .= "Reply-To: " . $name . ' <' . $email . '> ' . "\r\n";
+
+			$mail_result = mail( $to, $subject, $body, $headers );
+
+			if ( $mail_result == true )
+				{ echo 'success'; }
+			else
+				{ echo 'unsuccess'; }
+		}
+		else // not validated
+		{
+			echo 'error';
+		}
+		// end if
+
+	}
+	else // BOT DETECTED - lets lie to it
+	{
+		//echo "Thanks, We'll get back to you as soon as possible";
+		echo 'success';
+	}
 
 ?>
